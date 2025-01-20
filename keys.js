49139b2dadb6dabd9fd5d02755487bfed978f40f
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-20 22:50:19 +0600

    keys of js

---
+++ keys.js
@@ -0,0 +1,11 @@
+const student={
+    name:"jahid Hasan",
+    age:23,
+    section:"A",
+    batch:37,
+    number:"01782885378"
+}
+const key=Object.keys(student);
+const value=Object.values(student)
+console.log(key)
+console.log(value)
