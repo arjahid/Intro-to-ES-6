91b1433b53d8cbcfe082405311ab1f7de671bbeb
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-20 20:22:41 +0600

    es6

---
+++ template.js
@@ -0,0 +1,5 @@
+const first='jaan';
+const last='pakhi';
+const greet='potas potas'
+const name=first+' '+last+' '+greet
+console.log(name);
---
+++ var-let-const.js
@@ -0,0 +1,4 @@
+const numbers=[1,2,3,4]
+numbers[1]=0;
+numbers.unshift(0)
+console.log(numbers)
