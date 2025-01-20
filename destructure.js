35bf17fbd6e0a5850a711b410b5d2cc413cc993d
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-20 22:27:09 +0600

    desteracture

---
+++ destructure.js
@@ -0,0 +1,20 @@
+const student={
+    name:"jahid Hasan",
+    age:23,
+    section:"A",
+    batch:37,
+    number:"01782885378"
+}
+const {name,age,section,batch,number}=student;
+console.log(number)
+console.log(section)
+console.log(age)
+//arary destracture
+const numbers=[45,99]
+const [x,y]=[12,66]
+// 
+const double=(a,b)=> {
+return[a*2,b*2] 
+};
+const [protom,ditio]=double(3,4);
+console.log(protom,ditio)
