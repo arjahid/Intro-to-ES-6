4c9a18b7d04c937eb9b2cd4789d3bb0671e92739
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-21 11:40:35 +0600

    maps function

---
+++ map.js
@@ -0,0 +1,13 @@
+// const numbers=[4,5,2,8,10];
+// const double=[];
+// for (let num of numbers){
+//     const doubl=num*2;
+//     double.push(doubl);
+// }
+// console.log(double)
+const numbers=[4,5,2,8,10];
+const numbers1=numbers.map(num=>num*2);
+console.log(numbers1)
+const friends=['tom','mom','jomd','komd','lomd'];
+const length=friends.map(frines=>frines.length); 
+console.log(length)
