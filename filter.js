f4de1abc4dd5a3f0dadfd4aec9719b6ca3467c49
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-21 11:53:39 +0600

    filter js

---
+++ filter.js
@@ -0,0 +1,7 @@
+const player=[75,65,67,72,55,59]
+// const selectede=player.filter(p=>p>60)
+// console.log(selectede)
+// const selectede=player.filter(p=>p>70)
+// console.log(selectede)
+const selectede=player.filter(p=>p%2 ===1)
+console.log(selectede)
