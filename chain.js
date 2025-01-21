6df8e34f613db025f4e36233154ee898fc0d6cea
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-21 11:26:33 +0600

    chain javascript

---
+++ chain.js
@@ -0,0 +1,11 @@
+const data=[{id:1,name:"jahid",age:34,address:"sirajgonj"}]
+console.log(data[0].name)
+const product={
+    count:45,
+    data:[
+        {id:1,name:"laptop",price:45000},
+        {id:2,name:"mobile",price:25000},
+        {id:3,name:"watch",price:5000}
+    ]
+}
+console.log(product.data[2].price)
