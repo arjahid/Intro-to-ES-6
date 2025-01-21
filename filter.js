cfe68c1521c2b07759e9d218f1fbf2e113675e7e
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-21 11:58:38 +0600

    reduce jd

--- filter.js
+++ filter.js
@@ -1,7 +1,7 @@
 const player=[75,65,67,72,55,59]
-// const selectede=player.filter(p=>p>60)
-// console.log(selectede)
-// const selectede=player.filter(p=>p>70)
-// console.log(selectede)
-const selectede=player.filter(p=>p%2 ===1)
+const selectede=player.filter(p=>p>60)
 console.log(selectede)
+const selectede1=player.filter(p=>p>70)
+console.log(selectede1)
+const selectede2=player.filter(p=>p%2 ===1)
+console.log(selectede2)
---
+++ reduce.js
@@ -0,0 +1,3 @@
+const number=[4,5,6,7,8];
+const result=number.reduce((prevuous,current)=>prevuous +current ,0)
+console.log(result);
