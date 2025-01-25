6a45a086f1a9c0bb48d659a1615a34f34ed777e8
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-26 00:13:09 +0600

    adding fundamental concept

---
+++ common-js-concept/fundemental.js
@@ -0,0 +1,14 @@
+//1.How to declallre a variable using let and const
+const fatherName='Ahshan ali';
+let season='rainy'
+season='winter'
+//2. 6 basic conditation >,<,===,!==,<=,>=
+ //multiple conditation : && , ||
+ if(fatherName==='ahshan' || season==='rainy'){
+
+ }else if(fatherName ==='Ahshan ali'){
+
+ }
+ else{
+    
+ }
