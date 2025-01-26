65c6023714f1f3a45174b0375ad7f7fa35445ba6
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-26 18:29:05 +0600

    added es6

--- common-js-concept/fundemental.js
+++ common-js-concept/fundemental.js
@@ -1,39 +1,39 @@
 //1.How to declallre a variable using let and const
-const fatherName='Ahshan ali';
-let season='rainy'
-season='winter'
+const fatherName = "Ahshan ali";
+let season = "rainy";
+season = "winter";
 //2. 6 basic conditation >,<,===,!==,<=,>=
- //multiple conditation : && , ||
- if(fatherName==='ahshan' || season==='rainy'){
-
- }else if(fatherName ==='Ahshan ali'){
-
- }
- else{
-
- }
- //3.array declare,index,length,push,pop
- const numbers=[23,345,56,56,78];
- numbers.pop()
- numbers.push(5,6)
-const l= numbers.length
-console.log(l)
+//multiple conditation : && , ||
+if (fatherName === "ahshan" || season === "rainy") {
+} else if (fatherName === "Ahshan ali") {
+} else {
+}
+//3.array declare,index,length,push,pop
+const numbers = [23, 345, 56, 56, 78];
+numbers.pop();
+numbers.push(5, 6);
+const l = numbers.length;
+console.log(l);
 //4.for loop
-for(let i=0;i<numbers.length;i++){
-    const num=numbers[i]
-    console.log(num)
+for (let i = 0; i < numbers.length; i++) {
+  const num = numbers[i];
+  console.log(num);
 }
 //5.function
-const mul=(p,q)=>{
-    return p*q
-}
-const output=mul(2,5)
-console.log(output)
+const mul = (p, q) => {
+  return p * q;
+};
+const output = mul(2, 5);
+console.log(output);
 //6.object
 //3 way to access proparty
-const student={
-    name:"jahid",
-    age:23,
-    like:['programming','developer']
-}
-console.log(student.like)
+const student = {
+  name: "jahid",
+  age: 23,
+  like: ["programming", "developer"],
+};
+console.log(student.like); //directlly
+console.log(student["like"]); //access via proparty name string
+const myVariable = "age";
+console.log(student[myVariable]); //access via proparty name in variable
+//es6
