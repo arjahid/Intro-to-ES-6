09a7f46a4767b9661ccd0e4f5ca02c2688ec7bab
Jahid-Hasan <jhjahidhasanar@gmail.com>
2025-01-28 13:02:34 +0600

    adding user data

---
+++ Reat/user.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function User() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>User:{user.length}</h1>
+    </div>
+  );
+}
